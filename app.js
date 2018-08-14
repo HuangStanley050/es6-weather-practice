038558974712f2173b9a8d885775de2935ef243d
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-14 12:51:36 +0000

    forgot to add Reflect.get() and Reflect.set() but problem fixed and app finished

--- app.js
+++ app.js
@@ -9,14 +9,27 @@ const searchWeather = () => {
         return alert("Please enter city name");
     }
     const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + CITY_NAME + '&units=metrics&appid=' + KEY;
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'block';
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'none';
+
     Http.fetchData(URL)
         .then(responseData => {
             console.log(responseData);
             const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
             const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
             WEATHER_PROXY.temperature = responseData.main.temp;
+            updateWeather(WEATHER_PROXY);
         })
         .catch(err => alert(err));
 };
+
+const updateWeather = (weatherData) => {
+    ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+    ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+    ELEMENTS.ELEMENT_WEATHER_TEMPEARATURE.textContent = weatherData.temperature;
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'block';
+    ELEMENTS.ELEMENT_LOADING_TEXT.style.display = 'none';
+
+}
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
--- weather-data.js
+++ weather-data.js
@@ -8,10 +8,10 @@ export class WeatherData {
 
 export const WEATHER_PROXY_HANDLER = {
     get: function(target, property) {
-        return Reflect(target, property);
+        return Reflect.get(target, property);
     },
     set: function(target, property, value) {
         const newValue = (value * 1.8 + 32).toFixed(2) + 'F.';
-        return Reflect(target, property, newValue);
+        return Reflect.set(target, property, newValue);
     }
 };
